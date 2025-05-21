52b6c5123f8afa1a287f59ffec2bf25773f24f41
xdjeremy <jeremy.fran@icloud.com>
2025-05-21 22:00:26 +0800

    Integrate sitemap and robots.txt support in Astro configuration; update Layout.astro for dynamic metadata and sitemap link; refactor index.astro to use Layout component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://timbuilding.com",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap(), robotsTxt()],
 });
